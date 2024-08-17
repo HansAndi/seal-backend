5f7773d5e69dd6075c87b26a4377eeac51cfdd4b
HansAndi <hansandiwijaya@gmail.com>
2024-08-17 09:23:46 +0700

    fix: user resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
@@ -18,7 +19,8 @@ class UserResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
-            'avatar' => $this->avatar,
+            'avatar' => $this->avatar && !(str_starts_with($this->avatar, 'http')) ?
+                Storage::url($this->avatar) : $this->avatar,
             'role' => $this->role->getRole(),
         ];
     }
